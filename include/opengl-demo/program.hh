5a6b0c706f348ad2f35819dc92d6ffb2eeee5d25
Anis Ladram <anis.ladram@epita.fr>
2019-12-14 18:30:30 +0100

    EBO and color change

--- include/opengl-demo/program.hh
+++ include/opengl-demo/program.hh
@@ -28,6 +28,13 @@ namespace opengl_demo
     };
 
     template <>
+    void program::put<glm::vec4>(const char* name, const glm::vec4& val)
+    {
+        glUniform4fv(loc(name), 1, &val[0]);
+        TEST_OPENGL_ERROR();
+    }
+
+    template <>
     void program::put<glm::mat4>(const char* name, const glm::mat4& val)
     {
         glUniformMatrix4fv(loc(name), 1, GL_FALSE, &val[0][0]);
--- src/main.cc
+++ src/main.cc
@@ -19,8 +19,26 @@ using namespace opengl_demo;
 
 float lastFrame = 0.0f;
 
-static std::string get_path(const std::string& path) {
-	return "../resources/" + path;
+static GLuint generate_vao(float *vertices, size_t vertices_size, unsigned *indices, size_t indices_size)
+{
+    GLuint my_vao, my_vbo, my_ebo;
+    glGenVertexArrays(1, &my_vao);
+    glGenBuffers(1, &my_vbo);
+    glGenBuffers(1, &my_ebo);
+
+    glBindVertexArray(my_vao);
+      glBindBuffer(GL_ARRAY_BUFFER, my_vbo);
+        glBufferData(GL_ARRAY_BUFFER, vertices_size, vertices, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+      glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+      // Must not unbind EBO after that: will stay bind for this VAO
+      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, my_ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices_size, indices, GL_STATIC_DRAW);
+    glBindVertexArray(0);
+
+    return my_vao;
 }
 
 int main()
@@ -34,31 +52,19 @@ int main()
 
     program program{compile_my_shaders()};
     program.use();
-#if 0
-    program.put("my_texture", 0);
-#endif
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
     };
 
-    GLuint my_vbo;
-    glGenBuffers(1, &my_vbo);
-
-    GLuint my_vao;
-    glGenVertexArrays(1, &my_vao);
-
-    glBindVertexArray(my_vao);
-    glBindBuffer(GL_ARRAY_BUFFER, my_vbo);
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+    GLuint my_vao = generate_vao(vertices, sizeof(vertices), indices, sizeof(indices));
 
     glm::mat4 projection = glm::perspective(
             glm::radians(45.f),
@@ -72,6 +78,8 @@ int main()
             glm::vec3(0.f, 1.f, 0.f)
         );
 
+    program.put("proj_view_mat", projection * view);
+
     while (!glfwWindowShouldClose(window))
     {
         float currentFrame = glfwGetTime();
@@ -84,15 +92,12 @@ int main()
         glClearColor(135.f / 255.f, 206.f / 255.f, 235.f / 255.f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        program.use();
-        glBindVertexArray(my_vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-#if 0
-        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        glm::mat4 view = camera.GetViewMatrix();
-#endif
+        float t = glfwGetTime();
 
-        program.put("proj_view_mat", projection * view);
+        program.use();
+        program.put("color", glm::vec4(0.f, (sin(t) / 2.0f) + 0.5f, 0.f, 0.f));
+        glBindVertexArray(my_vao);
+        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(*indices), GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -101,4 +106,3 @@ int main()
     glfwTerminate();
     return 0;
 }
-
