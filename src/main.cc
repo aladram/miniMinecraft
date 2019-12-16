0e4d2f0d53eec5347d4d90977409fb67d54d8e7f
Anis Ladram <anis.ladram@epita.fr>
2019-12-16 02:25:02 +0100

    Texture application

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,18 @@
+extern "C" {
+#include <err.h>
+}
+#include <array>
+#include <cstdio>
+#include <cstring>
+#include <map>
+#include <numeric>
+#include <stdexcept>
+#include <vector>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -9,13 +22,10 @@
 #include <opengl-demo/program.hh>
 #include <opengl-demo/window.hh>
 
-#include <array>
-#include <map>
-#include <numeric>
-#include <stdexcept>
-#include <vector>
-
 using namespace opengl_demo;
+
+extern unsigned char container_jpg_buf[0];
+extern unsigned int container_jpg_buf_len;
 
 float lastFrame = 0.0f;
 
@@ -29,8 +39,10 @@ static GLuint generate_vao(float *vertices, size_t vertices_size, unsigned *indi
     glBindVertexArray(my_vao);
       glBindBuffer(GL_ARRAY_BUFFER, my_vbo);
         glBufferData(GL_ARRAY_BUFFER, vertices_size, vertices, GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), NULL);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) (3 * sizeof(float)));
         glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
       glBindBuffer(GL_ARRAY_BUFFER, 0);
 
       // Must not unbind EBO after that: will stay bind for this VAO
@@ -39,6 +51,20 @@ static GLuint generate_vao(float *vertices, size_t vertices_size, unsigned *indi
     glBindVertexArray(0);
 
     return my_vao;
+}
+
+static GLuint generate_texture(void *buf, unsigned buf_size)
+{
+    int width, height, nrChannels;
+
+    unsigned char *data = stbi_load_from_memory((const stbi_uc *)buf, (int)buf_size, &width, &height, &nrChannels, 0);
+    if (!data)
+        errx(1, "Texture load failed");
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(data);
 }
 
 int main()
@@ -54,10 +80,11 @@ int main()
     program.use();
 
     float vertices[] = {
-        0.5f,  0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
+        // positions         // texture coords
+        0.5f,  0.5f, 0.0f,   1.0f, 1.0f,   // top right
+        0.5f, -0.5f, 0.0f,   1.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f,   // bottom left
+        -0.5f,  0.5f, 0.0f,  0.0f, 1.0f    // top left
     };
     unsigned indices[] = {  // note that we start from 0!
         0, 1, 3,   // first triangle
@@ -65,6 +92,18 @@ int main()
     };
 
     GLuint my_vao = generate_vao(vertices, sizeof(vertices), indices, sizeof(indices));
+
+    unsigned int my_texture;
+    glGenTextures(1, &my_texture);
+
+    glBindTexture(GL_TEXTURE_2D, my_texture);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);   
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+      generate_texture(container_jpg_buf, container_jpg_buf_len);
+    glBindTexture(GL_TEXTURE_2D, 0);
 
     glm::mat4 projection = glm::perspective(
             glm::radians(45.f),
@@ -96,6 +135,7 @@ int main()
 
         program.use();
         program.put("color", glm::vec4(0.f, (sin(t) / 2.0f) + 0.5f, 0.f, 0.f));
+        glBindTexture(GL_TEXTURE_2D, my_texture);
         glBindVertexArray(my_vao);
         glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(*indices), GL_UNSIGNED_INT, 0);
 
